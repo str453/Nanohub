1cd6676bcb3e47e254bf1b2be8c9cec3572b165c
kehknee <kennethquanminhtran@gmail.com>
2025-10-05 22:45:18 -0700

    Added login page container and layout

--- src/Pages/LoginSignUp.jsx
+++ src/Pages/LoginSignUp.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import './CSS/LoginSignup.css'
 
 export const LoginSignUp = () => {
   return (
-    <div>
-      
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your Name'/>
+          <input type="email" placeholder='Email Address'/>
+          <input type="password" placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">Already have an account?<span> Login</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name='' id=''/>
+          <p>By continuing, you agree to the terms and privacy policy.</p>
+        </div>
+      </div>
     </div>
   )
 }
