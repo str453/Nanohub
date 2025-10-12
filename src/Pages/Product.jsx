2594d3f7b52d410020db8c2abde465cedcaa63e4
str453 <89546456+str453@users.noreply.github.com>
2025-10-12 19:57:35 +0000

    Added part of the item access

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import { useParams } from 'react-router-dom'
+import { Breadcrum } from '../Components/Breadcrums/Breadcrum.jsx'
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay.jsx'
 
 export const Product = () => {
+  const {all_product}= useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_product.find((e)=> e.id === Number(productId));
+
+  if (!product) {
+        // You can return a loading indicator or null while the data loads
+        // Returning 'null' prevents a crash while React waits for the next render
+        return null; 
+    }
+    
   return (
     <div>
-        
+        <Breadcrum product={product}/>
+        <ProductDisplay product={product}/>
     </div>
   )
 }
