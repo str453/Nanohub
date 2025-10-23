524c5126c31839548b6f4c0eb2d8882594b19909
kehknee <kennethquanminhtran@gmail.com>
2025-10-22 23:40:18 -0700

    Updated CSS for product display and breadcrums

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -3,7 +3,8 @@ import './ProductDisplay.css'
 import star_icon from "../Assets/star_icon.png";
 import star_dull_icon from "../Assets/star_dull_icon.png";
 
-const ProductDisplay = ({product}) => {
+const ProductDisplay = (props) => {
+      const {product} = props;
     return (
         <div className='productdisplay'>
             <div className="productdisplay-left">
@@ -19,7 +20,7 @@ const ProductDisplay = ({product}) => {
             </div>
             <div className="productdisplay-right">
                 <h1>{product.name}</h1>
-                <div className="productdisplay-right-star">
+                <div className="productdisplay-right-stars">
                   <img src={star_icon} alt="" />
                   <img src={star_icon} alt="" />
                   <img src={star_icon} alt="" />
@@ -27,6 +28,26 @@ const ProductDisplay = ({product}) => {
                   <img src={star_dull_icon} alt="" />
                   <p>(122)</p>
                 </div>
+                <div className="productdisplay-right-prices">
+                  <div className="productdisplay-right-price-old">${product.old_price}</div>
+                  <div className="productdisplay-right-price-new">${product.new_price}</div>
+                </div>
+                <div className="productdisplay-right-descrption">
+                  Description Here
+                </div>
+                <div className="productdisplay-right-size">
+                  <h1>Select Type</h1>
+                  <div className="productdisplay-right-sizes">
+                    <div>Small</div>
+                    <div>Medium</div>
+                    <div>Large</div>
+                    <div>XL</div>
+                    <div>XXL</div>
+                  </div>
+                </div>
+                <button>Add To Cart</button>
+                <p className='productdisplay-right-category'><span>Category: </span>Women , T-Shirt, Croptop</p>
+                <p className='productdisplay-right-category'><span>Tags: </span>Modern, Latest</p>
             </div>
         </div>
     )
