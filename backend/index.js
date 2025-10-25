2065c9537b873d03a6ebda8ff71e01b679a1d3f7
str453 <89546456+str453@users.noreply.github.com>
2025-10-25 21:46:25 +0000

    Added image upload

---
+++ backend/index.js
@@ -0,0 +1,54 @@
+const port = 4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const muter = require("multer");
+const path = require("path");
+const cors = require("cors");
+const multer = require("multer");
+const { createBrotliCompress } = require("zlib");
+
+app.use(express.json());
+app.use(cors());
+
+// Database connection With MongoDB
+mongoose.connect("mongodb+srv://HubertTran:Nanohub@nanohub.n7h67a7.mongodb.net/");
+
+// API Creation
+
+app.get("/",(req, res)=>{
+    res.send("Express App is Running")
+})
+
+//Image Storage Engine
+
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename:(req,file,cv)=>{
+        return createBrotliCompress(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({storage:storage})
+
+// Creating Upload Endpoint for images
+app.use('/images',express.static('upload/images'))
+
+app.post("/upload",upload.single('product'),(req,res)=>{
+    res.json({
+        success:1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+app.listen(port, (error)=>{
+    if(!error) {
+        console.log("Server Running on Port "+port)
+    }
+    else
+    {
+        console.log("Error: "+error)
+    }
+})
+
