1bff1ed6ebf42f8c8983e1e7e327773fbb3a7859
str453 <89546456+str453@users.noreply.github.com>
2025-10-27 20:39:18 +0000

    Added req. comments and commented out index.js from backend

--- backend/index.js
+++ backend/index.js
@@ -1,141 +1,141 @@
-const port = 4000;
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
-const muter = require("multer");
-const path = require("path");
-const cors = require("cors");
-const multer = require("multer");
-const { createBrotliCompress } = require("zlib");
-const { type } = require("os");
+// const port = 4000;
+// const express = require("express");
+// const app = express();
+// const mongoose = require("mongoose");
+// const jwt = require("jsonwebtoken");
+// const muter = require("multer");
+// const path = require("path");
+// const cors = require("cors");
+// const multer = require("multer");
+// const { createBrotliCompress } = require("zlib");
+// const { type } = require("os");
 
-app.use(express.json());
-app.use(cors());
+// app.use(express.json());
+// app.use(cors());
 
-// Database connection With MongoDB
-mongoose.connect("mongodb+srv://HubertTran:Nanohub@nanohub.n7h67a7.mongodb.net/");
+// // Database connection With MongoDB
+// mongoose.connect("mongodb+srv://HubertTran:Nanohub@nanohub.n7h67a7.mongodb.net/");
 
-// API Creation
+// // API Creation
 
-app.get("/",(req, res)=>{
-    res.send("Express App is Running")
-})
+// app.get("/",(req, res)=>{
+//     res.send("Express App is Running")
+// })
 
-//Image Storage Engine
+// //Image Storage Engine
 
-const storage = multer.diskStorage({
-    destination: './upload/images',
-    filename:(req,file,cv)=>{
-        return createBrotliCompress(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-    }
-})
+// const storage = multer.diskStorage({
+//     destination: './upload/images',
+//     filename:(req,file,cv)=>{
+//         return createBrotliCompress(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+//     }
+// })
 
-const upload = multer({storage:storage})
+// const upload = multer({storage:storage})
 
-// Creating Upload Endpoint for images
-app.use('/images',express.static('upload/images'))
+// // Creating Upload Endpoint for images
+// app.use('/images',express.static('upload/images'))
 
-app.post("/upload",upload.single('product'),(req,res)=>{
-    res.json({
-        success:1,
-        image_url:`http://localhost:${port}/images/${req.file.filename}`
-    })
-})
+// app.post("/upload",upload.single('product'),(req,res)=>{
+//     res.json({
+//         success:1,
+//         image_url:`http://localhost:${port}/images/${req.file.filename}`
+//     })
+// })
 
-// Schema for Creating Products
+// // Schema for Creating Products
 
-const Product = mongoose.model("Product",{
-    id:{
-        type:Number,
-        required:true,
-    },
-    name:{
-        type:String,
-        required:true,
-    },
-    image:{
-        type:String,
-        required:true,
-    },
-    category:{
-        type:String,
-        required:true,
-    },
-    new_price:{
-        type:Number,
-        required:true,
-    },
-    old_price:{
-        type:Number,
-        required:true,
-    },
-    date:{
-        type:Date,
-        default:Date.now
-    },
-    available:{
-        type:Boolean,
-        required:true,
-    },
+// const Product = mongoose.model("Product",{
+//     id:{
+//         type:Number,
+//         required:true,
+//     },
+//     name:{
+//         type:String,
+//         required:true,
+//     },
+//     image:{
+//         type:String,
+//         required:true,
+//     },
+//     category:{
+//         type:String,
+//         required:true,
+//     },
+//     new_price:{
+//         type:Number,
+//         required:true,
+//     },
+//     old_price:{
+//         type:Number,
+//         required:true,
+//     },
+//     date:{
+//         type:Date,
+//         default:Date.now
+//     },
+//     available:{
+//         type:Boolean,
+//         required:true,
+//     },
 
-})
+// })
 
-app.post('/add-product',async(req,res)=>{
-    let products = await Product.find({});
-    let id;
-    if(product.length>0)
-    {
-        let last_product_array = products.slice(1);
-        let last_product = last_product_array[0];
-        id = last_product.id+1;
-    }
-    else{
-        id = 1;
-    }
-    const product = new Product({
-        id:id,
-        name:req.body.name,
-        image:req.body.image,
-        category:req.body.category,
-        new_price:req.body.new_price,
-        old_price:req.body.old_price,
-    });
-    console.log(product);
-    await product.save();
-    console.log("Saved");
-    res.json({
-        success:true,
-        name:req.body.name,
-    })
-})
+// app.post('/add-product',async(req,res)=>{
+//     let products = await Product.find({});
+//     let id;
+//     if(product.length>0)
+//     {
+//         let last_product_array = products.slice(1);
+//         let last_product = last_product_array[0];
+//         id = last_product.id+1;
+//     }
+//     else{
+//         id = 1;
+//     }
+//     const product = new Product({
+//         id:id,
+//         name:req.body.name,
+//         image:req.body.image,
+//         category:req.body.category,
+//         new_price:req.body.new_price,
+//         old_price:req.body.old_price,
+//     });
+//     console.log(product);
+//     await product.save();
+//     console.log("Saved");
+//     res.json({
+//         success:true,
+//         name:req.body.name,
+//     })
+// })
 
-// API for delete products
+// // API for delete products
 
-app.post('/removeproduct', async (req,res)=>{
-    await Product.findOneAndDelete({id:req.body.id});
-    console.log("Removed");
-    res.json({
-        success:true,
-        name:req.body.name
-    })
-})
+// app.post('/removeproduct', async (req,res)=>{
+//     await Product.findOneAndDelete({id:req.body.id});
+//     console.log("Removed");
+//     res.json({
+//         success:true,
+//         name:req.body.name
+//     })
+// })
 
-// API for get all products
+// // API for get all products
 
-app.get('/allproducts',async (req,res)=>{
-    let products = await Product.find({});
-    console.log("All Products Fetched");
-    res.send(products);
-})
+// app.get('/allproducts',async (req,res)=>{
+//     let products = await Product.find({});
+//     console.log("All Products Fetched");
+//     res.send(products);
+// })
 
-app.listen(port, (error)=>{
-    if(!error) {
-        console.log("Server Running on Port "+port)
-    }
-    else
-    {
-        console.log("Error: "+error)
-    }
-})
+// app.listen(port, (error)=>{
+//     if(!error) {
+//         console.log("Server Running on Port "+port)
+//     }
+//     else
+//     {
+//         console.log("Error: "+error)
+//     }
+// })
 
